93274c894ebe7394273370dcd857044518cf6cec
Jose Villalobos <jose.villalobos@geniussports.com>
2022-01-14 10:33:49 -0500

    Minor refactor.

--- src/main/kotlin/com/jose/hellodgs/HellodgsApplication.kt
+++ src/main/kotlin/com/jose/hellodgs/HellodgsApplication.kt
@@ -28,27 +28,6 @@ class HellodgsApplication
 // Example datafetcher:
 // https://github.com/Netflix/dgs-examples-kotlin/blob/main/src/main/kotlin/com/example/demo/datafetchers/ShowsDataFetcher.kt
 
-@DgsComponent
-class ShowsDataFetcher {
-	private val shows = listOf(
-		Show("Stranger Things", 2016),
-		Show("Ozark", 2017),
-		Show("The Crown", 2016),
-		Show("Dead to Me", 2019),
-		Show("Orange is the New Black", 2013))
-
-	@DgsQuery
-	fun shows(@InputArgument titleFilter : String?): List<Show> {
-		return if(titleFilter != null) {
-			shows.filter { it.title.contains(titleFilter) }
-		} else {
-			shows
-		}
-	}
-
-	data class Show(val title: String, val releaseYear: Int)
-}
-
 fun main(args: Array<String>) {
 	runApplication<HellodgsApplication>(*args)
 }
