023017e65fcc4abe5606c98bbdab772916d32ec0
Jose Villalobos <jose.villalobos@geniussports.com>
2022-01-14 11:35:56 -0500

    Minor refactors on names.

--- src/main/kotlin/com/jose/hellodgs/datafetchers/ShowsDataFetcher.kt
+++ src/main/kotlin/com/jose/hellodgs/datafetchers/ShowsDataFetcher.kt
@@ -1,17 +1,18 @@
-package com.jose.hellodgs.controllers
+package com.jose.hellodgs.datafetchers
 
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsQuery
 import com.netflix.graphql.dgs.InputArgument
 
 @DgsComponent
-class ShowsController {
+class ShowsDataFetcher {
     private val shows = listOf(
         Show("Stranger Things", 2016),
         Show("Ozark", 2017),
         Show("The Crown", 2016),
         Show("Dead to Me", 2019),
-        Show("Orange is the New Black", 2013))
+        Show("Orange is the New Black", 2013)
+    )
 
     @DgsQuery
     fun shows(@InputArgument titleFilter : String?): List<Show> {
